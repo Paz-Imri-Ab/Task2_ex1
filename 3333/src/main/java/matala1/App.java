af6d3bfbc6af784aa20035988031514e52b4aa82
pazimriabutbul <pazimriabutbul@192.168.1.25>
2024-01-21 15:31:20 +0200

    version-3

--- 3333/src/main/java/matala1/App.java
+++ 3333/src/main/java/matala1/App.java
@@ -1,5 +1,7 @@
 package matala1;
 
+
+
 /**
  * Hello world!
  *
@@ -7,8 +9,44 @@ package matala1;
 public class App 
 {
 	
+	public static String NumToChar(int Grade) 
+	{
+		if(Grade >= 1 && Grade <=49)
+		{
+			return "F";
+		}
+		
+		else if(Grade >= 50 && Grade <= 59) 
+		{
+			return "E";
+		}
+		
+		else if(Grade >= 60 && Grade <= 69) 
+		{
+			return "D";
+		}
+		
+		else if(Grade >= 70 && Grade <= 79) 
+		{
+			return "C";
+		}
+		
+		else if(Grade >= 80 && Grade <= 89) 
+		{
+			return "B";
+		}
+		
+		else if(Grade >= 90 && Grade <= 100) 
+		{
+			return "A";
+		}
+		
+		return "ERROR";
+	}
+	
+	
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        
     }
 }
--- 3333/src/test/java/matala1/AppTest.java
+++ 3333/src/test/java/matala1/AppTest.java
@@ -1,5 +1,6 @@
 package matala1;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
@@ -17,4 +18,14 @@ public class AppTest
     {
         assertTrue( true );
     }
+    
+    @Test
+    public void exe_2_Partition_F_min()
+    {
+    	String result = App.NumToChar(1);
+    	String expected_result = "F";
+    	assertEquals(result, expected_result);
+    }
+    
+ 
 }
